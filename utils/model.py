dbc32ca14a074fb2ca73f880954847d52c929d73
david <adamita.david@gmail.com>
2025-10-18 01:07:24 +0300

    Added recommendations for user query

--- utils/model.py
+++ utils/model.py
@@ -32,6 +32,7 @@ def kmeans_model(df):
 
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(X_clean)
+    df_clean['audio_embedding'] = list(X_scaled)
 
     pca = PCA(n_components = 3)
     pca.fit(X_scaled)
@@ -76,6 +77,30 @@ def create_lyrics_embedding(df, model):
     df['lyrics_embedding'] = df['clean_lyrics'].apply(lambda x: model.encode(x))
     return df
 
+def recommend_by_lyrics(df):
+
+    song_name = df.sample(1).iloc[0]['track_name']
+    print(f'Similar songs to {song_name}')
+    query_vec = df.loc[df['track_name'] == song_name, 'lyrics_embedding'].values[0]
+    lyrics_matrix = np.stack(df['lyrics_embedding'].values)
+    similarities = cosine_similarity([query_vec], lyrics_matrix)[0]
+
+    df['similarities'] = similarities
+    recommendations = df.sort_values('similarities', ascending = False).head(6).iloc[1:][['track_name', 'track_artist', 'similarities']]
+    return recommendations
+
+def recommend_by_query(df, query, model):
+    
+    print(query)
+    query_vec = model.encode(query)
+    lyrics_matrix = np.stack(df['lyrics_embedding'].values)
+    similarities = cosine_similarity([query_vec], lyrics_matrix)[0]
+
+    df['similarities'] = similarities
+    recommendations = df.sort_values('similarities', ascending = False).head(5)[['track_name', 'track_artist', 'similarities']]
+    return recommendations
+
+
 if __name__ == "__main__":
 
     df = pd.read_csv('../data/sample1000_with_lyrics.csv')
@@ -92,7 +117,7 @@ if __name__ == "__main__":
 
     model = SentenceTransformer('../models/finetuned_bert')
     df = create_lyrics_embedding(df, model)
-    print(df.head())
+    #print(df.head())
 
     """
     s = 0
@@ -101,5 +126,8 @@ if __name__ == "__main__":
         s2 = df[df['label'] == 1].sample(1).iloc[0]['lyrics_embedding']
         similarity = cosine_similarity([s1], [s2])[0][0]
         print(similarity)
-        
+
     """
+
+    recommendations = recommend_by_query(df, 'Hey! I want some rock songs', model)
+    print(recommendations)
